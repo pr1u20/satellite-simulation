ece74b9bed2c7068db14d527103f33a8df412508
Pedro Rodriguez <pr1u20@soton.ac.uk>
2023-08-06 23:29:06 +0100

    Testing force and torque

--- utils.py
+++ utils.py
@@ -12,14 +12,16 @@ cubeStartOrientation = p.getQuaternionFromEuler([0,0,0])
 boxId = p.loadURDF("r2d2.urdf",cubeStartPos, cubeStartOrientation)
 
 mass, _, CoG, *_ = p.getDynamicsInfo(boxId, -1)
-print(mass)
+print(CoG)
+print(p.getDynamicsInfo(boxId, -1))
 
 ##
 
 for i in range (10000):
     p.stepSimulation()
     cubePos, cubeOrn = p.getBasePositionAndOrientation(boxId)
-    p.applyExternalForce(boxId, -1, (0, 10 , 0), -np.array(CoG), p.LINK_FRAME) # in Newton # WORLD_FRAME p.LINK_FRAME
+    #p.applyExternalForce(boxId, -1, (1, 0, 0), [0,0,0], p.LINK_FRAME) # in Newton # WORLD_FRAME p.LINK_FRAME
+    p.applyExternalTorque(boxId, -1, (0, 10, 0), p.LINK_FRAME)
     time.sleep(1./240.)
 
 cubePos, cubeOrn = p.getBasePositionAndOrientation(boxId)
